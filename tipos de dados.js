dd4f6982b4a78dc884eb8bdd83116f716365b393
Design Tech TI <mrc_490@hotmail.com>
2020-07-08 20:17:14 -0300

    Tipos de Dados

---
+++ tipos de dados.js
@@ -0,0 +1,39 @@
+// Tipos de Dados em JavaScript
+
+// string (texto);
+	var aluno = "Marcelo Junior";
+	var turma = 1;
+	const hub = "Sem Hub";
+
+// number (números inteiros ou decimais);
+	var idade = 22;
+	var salario = 3500.70;
+	var aumento = 1000.50;
+
+// booleanos (true or false);
+	var aprovado = true;
+	var aprovado = false;
+
+// null (nula);
+	var string = null;
+
+// objetos;
+
+	/* 	
+		Podem ser objetos: 
+		imagens, botões, 
+		formulários, parágrafos, 
+		títulos de páginas, etc...
+
+	*/
+
+// funções
+	// Cria a função
+	function helloWorld(parâmetros){
+		return "Olá mundo!";
+	}
+
+	// Chama a função
+	console.log(helloWorld()); // Olá mundo!
+
+
---
+++ variáveis & constantes.js
@@ -0,0 +1,25 @@
+// Variáveis & Constantes
+
+/* 
+
+Para criar VARIÁVEIS
+deve-se utilizar a 
+palavra reservada "var"
+
+*/
+
+var nomeCompleto = "Marcelo Junior";
+var idade = 22;
+var peso = "75.5 Kg";
+var altura = "185 cm";
+
+/* 
+
+Para criar CONSTANTES, isto é, 
+dados que não variam/mudam
+deve-se utilizar a 
+palavra reservada "const"
+
+*/
+
+const naturalidade = "Brasileiro";
