ad5c5ebefb7b641ca33c20941270f06c3cf8bb1a
Design Tech TI <mrc_490@hotmail.com>
2020-07-09 13:45:57 -0300

    Estruturas Condicionais
    If & Else

---
+++ estruturas condicionais.js
@@ -0,0 +1,23 @@
+// Estruturas Condicionais
+
+// Exemplo 1 - Nota do Desafio Trybe
+
+var notaDesafio = 150;
+
+if (notaDesafio >= 100) {
+	console.log("Aprovado");
+} else {
+	console.log("Não Aprovado");
+}
+
+// Exemplo 2 - Semáforo
+
+var luzSemaforo = "verde";
+
+if (luzSemaforo === "verde") {
+	console.log("Siga em frente!");
+} else if (luzSemaforo === "amarelo") {
+	console.log("Atenção! A sinal ficará vermelho");
+} else {
+	console.log("Pare! Sinal vermelho!")
+}
