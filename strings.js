ac3af27ad86959128750999f8849161c227f5b10
Design Tech TI <mrc_490@hotmail.com>
2020-07-09 17:49:44 -0300

    Strings
    Concatenação de strings

---
+++ strings.js
@@ -0,0 +1,20 @@
+// Strings
+
+// Concatenação de strings
+var primeiroNome = "Marcelo";
+var ultimoNome = "Junior";
+
+nomeCompleto = primeiroNome + " " + ultimoNome; // Retorna o valor Marcelo Junior
+
+// Exercício
+
+	// (a) Crie uma variável que contenha uma string com o nome do mÊs que você nasceu
+		var mêsDeNascimento = "Julho";
+
+	// (b) Acesse a primeira, segunda e terceira posição dessa string
+		mêsDeNascimento[0]; // Retorna o valor "J"
+		mêsDeNascimento[1]; // Retorna o valor "u"
+		mêsDeNascimento[2]; // Retorna o valor "l"
+
+	// (c) Concatene as três posições na mesma sequência
+		mêsConcatenado = mêsDeNascimento[0] + mêsDeNascimento[1] + mêsDeNascimento[2];	
