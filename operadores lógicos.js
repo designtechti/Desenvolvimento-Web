51d630a039fabef93cee70f3921e4083b697750c
Design Tech TI <mrc_490@hotmail.com>
2020-07-08 21:23:28 -0300

    Operadores Aritméticos & de Comparação

---
+++ operadores lógicos.js
@@ -0,0 +1,16 @@
+// Operadores Lógicos
+
+// Retorna sempre valores booleanos
+
+10 > 5 // Maior
+25 >= 25 // Maior ou igual
+5 < 42 // Menor
+28.90 <= 29 // Menor ou Igual
+30 === 30 // Igual
+1 !== 100 // Desigual
+
+var notaDaJoana = 125;
+var joanaPassouNoDesafio = notaDaJoana >= 100;
+
+var notaDaMaria = 95;
+var mariaPassouNoDesafio = notaDaMaria >= 100;
